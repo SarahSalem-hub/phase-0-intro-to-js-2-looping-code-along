99c50b72c52f2eb6adbbe7e631dee4bba9202caa
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-24 17:29:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// for (let age = 30;age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//   debugger;
+// }
+
+function writeCards(names,event){
+    let card = []
+    for (let i=0; i< names.length; i++){
+        card.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return card
+}
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+function countDown(num){
+    while (num > -1){
+        console.log(num)
+        num--;
+    }
+    
+}
+countDown(10)
